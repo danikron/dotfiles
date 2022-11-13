8c487ff070fe07bf0feffb74d5e370438650df8b
danikron <daniel.kronberg@gmail.com>
2022-11-13 01:50:40 +0100

    Fixed typo

--- sysscripts/battwarn.sh
+++ sysscripts/battwarn.sh
@@ -4,7 +4,7 @@
 
 if [[ COUNT -ge 300 ]]; then
 	COUNT=0
-	[[ $(pacman -Qi zenity) ]] && zenity --warning --text "<span font_size='large' font_weight='bold'>Low battery</span>\n$BATT% of battery remaining" --title "Low battery warning" --width=150 &> /dev/null
+	[[ $(pacman -Qi zenity) ]] && zenity --warning --text "<span font_size='large' font_weight='bold'>Low battery</span>\n$BATTERY% of battery remaining" --title "Low battery warning" --width=150 &> /dev/null
 fi
 
 ((COUNT++))
