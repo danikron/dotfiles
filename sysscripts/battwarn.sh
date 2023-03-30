1c6bdf53caa426a801d06faf8313bcf9776c14cb
danikron <daniel.kronberg@gmail.com>
2023-03-30 19:08:30 +0200

    Tweaked battery warning again.

--- sysscripts/battwarn.sh
+++ sysscripts/battwarn.sh
@@ -2,7 +2,7 @@
 
 [[ -f /tmp/battwarn ]] && COUNT=$(cat /tmp/battwarn)
 
-if [[ COUNT -ge 200 ]]; then
+if [[ COUNT -ge 200 || ! -f /tmp/battwarn ]]; then
 	COUNT=0
 	[[ $(pacman -Qi zenity) ]] && zenity --warning --text "<span font_size='large' font_weight='bold'>Low battery</span>\n$BATTERY% of battery remaining" --title "Low battery warning" --width=150 &> /dev/null
 fi
