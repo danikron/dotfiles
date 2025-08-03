101317172ba4fb1dd35b20f846360953f7dfe8b7
danikron <github@danikron.se>
2025-08-03 13:08:19 +0200

    Don't print gpu data unless nvidia-smi present

--- sysscripts/gpumon.sh
+++ sysscripts/gpumon.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-GPU_STATS=$(nvidia-smi --query-gpu=utilization.gpu,utilization.memory,temperature.gpu --format=csv,noheader,nounits)
-IFS=', ' read -r -a GPU_STAT_ARRAY <<< "$GPU_STATS"
+NVIDIA_SMI_PRESENT=$(which nvidia-smi 2>&1 >/dev/null && echo TRUE)
 
-printf '%s%%  %s%%  %s°C \n' "${GPU_STAT_ARRAY[0]}" "${GPU_STAT_ARRAY[1]}" "${GPU_STAT_ARRAY[2]}"
+if [ $NVIDIA_SMI_PRESENT = 'TRUE' ]; then
+	GPU_STATS=$(nvidia-smi --query-gpu=utilization.gpu,utilization.memory,temperature.gpu --format=csv,noheader,nounits)
+	IFS=', ' read -r -a GPU_STAT_ARRAY <<< "$GPU_STATS"
+
+	printf '%s%%  %s%%  %s°C \n' "${GPU_STAT_ARRAY[0]}" "${GPU_STAT_ARRAY[1]}" "${GPU_STAT_ARRAY[2]}"
+fi
