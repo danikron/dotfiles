f9edb79d1c615fa2f41ddd7d445fb849ee0fd356
danikron <github@danikron.se>
2025-08-17 17:08:39 +0200

    Removed malfunctoining shortcut

--- homedir/config/nvim/lua/plugins/blink.lua
+++ homedir/config/nvim/lua/plugins/blink.lua
@@ -13,13 +13,12 @@ return {
 				['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
 				['<C-e>'] = { 'hide' },
 				['<C-y>'] = { 'select_and_accept' },
-				['<C-f>'] = { 'select_and_accept' },
 
 				['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
 				['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
-				['<C-B>'] = { 'scroll_documentation_up', 'fallback' },
-				['<C-F>'] = { 'scroll_documentation_down', 'fallback' },
+				['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+				['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
 				['<Tab>'] = { 'snippet_forward', 'fallback' },
 				['<S-Tab>'] = { 'snippet_backward', 'fallback' },
