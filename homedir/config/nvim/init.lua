b80f362c16cecb2889029e1a6a10ab5b27ed603b
danikron <github@danikron.se>
2025-08-17 11:41:27 +0200

    Additional plugins

--- homedir/config/nvim/init.lua
+++ homedir/config/nvim/init.lua
@@ -153,10 +153,10 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 -- Splitting & Resizing
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+-- vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+-- vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+-- vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+-- vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
 -- Move lines up/down
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
---
+++ homedir/config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		'mbbill/undotree',
+		branch = 'master',
+		lazy = true,
+		keys = {
+			{ '<leader>u', "<cmd>UndotreeToggle<CR>" }
+		},
+		config = function()
+			vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>')
+			vim.g.undotree_WindowLayout = 4
+		end
+	}
+}
